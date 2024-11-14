5f456ce831cad7d5c8b759d7461d635ae4a5fc96
jackboom2510 <58967934+jackboom2510@users.noreply.github.com>
2024-11-14 09:41:05 +0700

    14/11 - Commit

--- #Other/_main.cpp
+++ #Other/_main.cpp
@@ -1,70 +1,23 @@
-// CPP0620 - SAP XEP SINH VIEN THEO LOP.cpp
-#include <bits/stdc++.h>
-#include <iostream>
-#include <algorithm>
-#include <cmath>
-#include <string>
-#include <vector>
-#include <iomanip>
-
-using namespace std;
-
-typedef long long ll;
-typedef long double ld;
-typedef pair<int,int> p32;
-typedef vector<ll> v64;
-typedef vector<int> v32;
-#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define f(i,s,e) for(int i = s; i < e; i++)
-#define rf(i,s,e) for(int i = s; i >= e; i--)
-#define ln "\n"
-#define INF 2e18
-#define all(x) (x).begin(), (x).end()
-#define mp make_pair
-#define pb push_back
-#define fi first
-#define se second
-
-typedef class SinhVien {
-private:
-	string msv, ht, l, email;
-public:
-	SinhVien() {
-		msv = ht = l = email = "";
-	}
-	friend istream& operator>>(istream& input, SinhVien& sv) {
-		input >> sv.msv;
-		getline(input, sv.ht);
-		while(sv.ht.size() == 0) getline(input, sv.ht);
-		input >> sv.l >> sv.email;
-		return input;
-	}
-	friend ostream& operator<<(ostream& output, const SinhVien& sv) {
-		output << sv.msv << " " << sv.ht << " " << sv.l << " " << sv.email;
-		return output;
-	}
-	friend bool operator<(const SinhVien& a, const SinhVien& b) {
-		if(a.l != b.l) return a.l < b.l;
-		return a.msv < b.msv;
-	}
-} SV;
-
-void solve()
-{
-	int N; cin >> N;
-	SV ds[N];
-	f(i, 0, N) cin >> ds[i];
-	sort(ds, ds+N);
-	f(i, 0, N) cout << ds[i] << ln;
-}
-
-int main()
-{
-	fast_cin();
-	int t = 1;
-	// cin >> t;
-	for(int it=1; it<=t; it++) {
-		solve();
-	}
-	return 0;
-}
+if(contains(prop("Price"), "₫ - "), 
+	toNumber(replaceAll(
+		slice(
+			prop("Price"),
+			findIndex(split(prop("Price"), ""), "₫") + 1,
+			findIndex(split(prop("Price"), ""), " - ")
+		),
+		",",
+		""
+	)),
+	toNumber(replaceAll(
+		slice(
+			prop("Price"),
+			findIndex(split(prop("Price"), ""), "₫") + 1,
+			length(prop("Price"))
+		),
+		",",
+		""
+		))
+)
+	
+₫15.000 - ₫21.000
+₫126.000
